cfaf102a93a75bde69727e2df7b3b814ed43419e
Abhinav Chauhan <abhinavchauhan2526@gmail.com>
2025-08-07 22:00:56 +0530

    Using puter.ai

--- app.js
+++ app.js
@@ -5,5 +5,19 @@ document.querySelector("#ask").addEventListener("click",async ()=>{
     return;
   }
   const ans=document.querySelector("#response");
-  ans.innerText=input;
+  ans.innerHTML="Thinking...";
+  const resp = puter.ai.chat(input,{model: "gpt-4.1-nano"},{stream : true});
+  resp.then(response=>{ 
+    ans.innerHTML=response.message.content;
+  })
+  .catch(error=>{
+    console.error("Error:", error);
+  });
+  document.querySelector("#inp").value="";
+});
+
+document.querySelector("#inp").addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    document.querySelector("#ask").click();
+  }
 });
